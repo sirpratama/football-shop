bb7ded14e242eba290e1e5acc8e1b94edc11008e
sirpratama <mnazirpratama@gmail.com>
2025-09-15 14:22:53 +0700

    Add static files configuration for production and update remote repository settings

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -142,6 +142,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Static files configuration for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
